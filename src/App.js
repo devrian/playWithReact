1ac65cb696405971486dacf412bf42c0d082ea62
devrian <deadcolony666@gmail.com>
2019-01-26 16:49:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// State
+class Timer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      time: props.start
+    }
+  }
+
+  //Lifecycle
+  componentDidMount() {
+    this.addInterval = setInterval(() => this.increase(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.addInterval)
+  }
+
+  increase() {
+    this.setState((state, props) => ({
+      time: parseInt(state.time) + 1
+    }))
+  }
+
+  render() {
+    return (
+      <div>{this.state.time} Second</div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Timer start="0"/>
         </header>
       </div>
     );
