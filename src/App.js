0d16018630eb79b2811ba85a257320f504efbb86
devrian <deadcolony666@gmail.com>
2019-01-26 17:07:52 +0700

    second play

--- src/App.js
+++ src/App.js
@@ -10,26 +10,59 @@ class Timer extends Component {
       time: props.start
     }
   }
-
   //Lifecycle
   componentDidMount() {
     this.addInterval = setInterval(() => this.increase(), 1000)
   }
-
   componentWillUnmount() {
     clearInterval(this.addInterval)
   }
-
   increase() {
     this.setState((state, props) => ({
       time: parseInt(state.time) + 1
     }))
   }
-
   render() {
     return (
       <div>{this.state.time} Second</div>
     );
+  }
+}
+
+function Clicker() {
+  function handleCLick(e) {
+    alert('I Got Click!')
+    e.preventDefault()
+  }
+
+  return (
+    <a href="#" onClick={handleCLick}>CLick Me!</a>
+  )
+}
+
+class Toggle extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      toggleStatus: true
+    }
+
+    this.handByClick = this.handByClick.bind(this)
+  }
+
+  handByClick() {
+    this.setState(state => ({
+      toggleStatus: !state.toggleStatus
+    }))
+  }
+
+  render() {
+    return (
+      <button onClick={this.handByClick}>
+        {this.state.toggleStatus ? 'ON' : 'OFF'} 
+        <p>{this.state.toggleStatus ? 'Enabled' : 'Disabled'} </p>
+      </button>
+    )
   }
 }
 
@@ -39,7 +72,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Timer start="0"/>
+          <Timer start="0"/> <br/>
+          <Clicker /> <br/>
+          <Toggle />
         </header>
       </div>
     );
